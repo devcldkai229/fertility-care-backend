db51320360265bfaffdafbec71fc37b876f966ad
Trung Nguyen <nguyen18nt@gmail.com>
2025-06-11 22:18:33 +0700

    fix: share add: TreatmentController

---
+++ FertilityCare.WebAPI/Controllers/TreatmentController.cs
@@ -0,0 +1,37 @@
+﻿using FertilityCare.UseCase.DTOs.TreatmentServices;
+using FertilityCare.UseCase.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FertilityCare.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TreatmentController : ControllerBase
+    {
+        private readonly IPublicTreatmentService _publicTreatmentService;
+        public TreatmentController(IPublicTreatmentService publicTreatmentService)
+        {
+            publicTreatmentService = _publicTreatmentService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse<IEnumerable<TreatmentServiceDTO>>>> GetAll()
+        {
+            try
+            {
+                var result = await _publicTreatmentService.GetAllAsync();
+                return Ok(new ApiResponse<IEnumerable<TreatmentServiceDTO>>
+                {
+                    StatusCode = 200,
+                    Message = "",
+                    Data = result,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+            catch (Exception ex)
+            {
+                return NoContent();
+            }
+        }
+    }
+}
