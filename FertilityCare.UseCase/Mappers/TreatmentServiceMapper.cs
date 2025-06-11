34e8e26ea1acbc5f77760c5510d51831e2424696
Trung Nguyen <nguyen18nt@gmail.com>
2025-06-11 21:58:16 +0700

    Add: Mapper and service treatment

---
+++ FertilityCare.UseCase/Mappers/TreatmentServiceMapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FertilityCare.Domain.Entities;
+using FertilityCare.UseCase.DTOs.TreatmentServices;
+using FertilityCare.UseCase.DTOs.TreatmentStep;
+
+namespace FertilityCare.UseCase.Mappers
+{
+    public static class TreatmentServiceMapper
+    {
+        public static TreatmentServiceDTO MapToTreatmentServiceDTO(this TreatmentService treatmentServices)
+        {
+            return new TreatmentServiceDTO
+            {
+                Id = treatmentServices.Id,
+                Name = treatmentServices.Name,
+                Description = treatmentServices.Description,
+                Duration = treatmentServices.Duration,
+                SuccessRate = treatmentServices.SuccessRate,
+                RecommendedFor = treatmentServices.RecommendedFor,
+                Contraindications = treatmentServices.Contraindications,
+                CreatedAt = treatmentServices.CreatedAt.ToString("dd/MM/yyyy HH:mm:ss"),
+                UpdatedAt = treatmentServices.UpdatedAt?.ToString("dd/MM/yyyy HH:mm:ss"),
+                TreatmentSteps = treatmentServices.TreatmentSteps?.Select(x => x.MapToTreatmentStepDTO()).ToList()
+
+            };
+        }
+    }
+}
