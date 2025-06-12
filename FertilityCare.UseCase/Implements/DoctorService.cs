c70bff77fb25c3c6a16eb96d8ca90d5baae5c94f
thanggumball <thanglatoiii@gmail.com>
2025-06-12 14:19:57 +0700

    add: DoctorService, IDoctorService

---
+++ FertilityCare.UseCase/Implements/DoctorService.cs
@@ -0,0 +1,38 @@
+﻿using FertilityCare.Domain.Entities;
+using FertilityCare.UseCase.DTOs.Doctors;
+using FertilityCare.UseCase.Interfaces.Repositories;
+using FertilityCare.UseCase.Interfaces.Services;
+using FertilityCare.UseCase.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FertilityCare.UseCase.Implements
+{
+    public class DoctorService : IDoctorService
+    {
+        private readonly IDoctorRepository _doctorRepository;
+
+        public DoctorService(IDoctorRepository doctorRepository)
+        {
+            _doctorRepository = doctorRepository;
+        }
+
+        public async Task<IEnumerable<DoctorDTO>> GetAllDoctorsAsync()
+        {
+            var result = await _doctorRepository.FindAllAsync();
+            return result.Select(x => x.MapToDoctorDTO()).ToList();
+        }
+
+        public async Task<DoctorDTO?> GetDoctorByIdAsync(string id)
+        {
+            if (!Guid.TryParse(id, out Guid doctorId))
+            return null;
+
+            var result = await _doctorRepository.FindByIdAsync(doctorId);
+            return result?.MapToDoctorDTO();
+        }
+    }
+}
---
+++ FertilityCare.UseCase/Interfaces/Services/IDoctorService.cs
@@ -0,0 +1,17 @@
+﻿using FertilityCare.Domain.Entities;
+using FertilityCare.UseCase.DTOs.Doctors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FertilityCare.UseCase.Interfaces.Services
+{
+    public interface IDoctorService
+    {
+        Task<IEnumerable<DoctorDTO>> GetAllDoctorsAsync();
+        Task<DoctorDTO?> GetDoctorByIdAsync(string id);
+
+    }
+}
