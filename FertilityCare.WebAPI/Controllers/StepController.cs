31208e78d94af4bca24cf11bfcda53852b20155b
devcldkai229 <kag40222@gmail.com>
2025-06-13 11:04:19 +0700

    feat: add GetStepsByOrderId api/v1/steps/orderId

---
+++ FertilityCare.WebAPI/Controllers/StepController.cs
@@ -0,0 +1,58 @@
+﻿using FertilityCare.Shared.Exceptions;
+using FertilityCare.UseCase.DTOs.OrderSteps;
+using FertilityCare.UseCase.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FertilityCare.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/steps")]
+    public class StepController : ControllerBase
+    {
+
+        private readonly IOrderStepService _stepService;
+
+        public StepController(IOrderStepService stepService)
+        {
+            _stepService = stepService;
+        }
+
+        [HttpGet]
+        [Route("/{orderId}")]
+        public async Task<ActionResult<IEnumerable<OrderStepDTO>>> GetStepsByOrderId([FromRoute] string orderId)
+        {
+            try
+            {
+                var result = await _stepService.GetAllStepsByOrderIdAsync(Guid.Parse(orderId));
+                return Ok(new ApiResponse<IEnumerable<OrderStepDTO>> {
+                    StatusCode = 200,
+                    Message = "Order steps fetched successfully!",
+                    Data = result,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+            catch(NotFoundException e)
+            {
+                return NotFound(new ApiResponse<object>
+                {
+                    StatusCode = e.StatusCode,
+                    Message = e.Message,
+                    Data = null,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new ApiResponse<object>
+                {
+                    StatusCode = 400,
+                    Message = ex.Message,
+                    Data = null,
+                    ResponsedAt = DateTime.Now
+                });
+            }
+        }
+
+
+    }
+}
