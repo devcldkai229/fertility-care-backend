39b3c530cf1bf9b305575e5c4b5e9a19a3880223
devcldkai229 <kag40222@gmail.com>
2025-06-15 23:52:34 +0700

    feat: Login api./v1/auth/login

---
+++ FertilityCare.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using FertilityCare.Infrastructure.Services;
+using FertilityCare.UseCase.DTOs.Auths;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FertilityCare.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResult>> Login([FromBody] LoginRequest request)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _authService.LoginAsync(request);
+            if(!result.IsSuccess)
+            {
+                return BadRequest(new { messsage = result.ErrorMessage });
+            }
+
+            return Ok(result.Data);
+        }
+
+    }
+}
