71920a4a77b7b3ffa927a35e65abd19dabdce76b
devcldkai229 <kag40222@gmail.com>
2025-06-15 19:37:43 +0700

    add: create new table RefreshToken

---
+++ FertilityCare.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FertilityCare.Domain.Entities
+{
+    public class RefreshToken
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Token { get; set; } = string.Empty;
+        public Guid UserId { get; set; }
+        public DateTime ExpiredAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
--- FertilityCare.Infrastructure/Services/IJwtService.cs
+++ FertilityCare.Infrastructure/Services/IJwtService.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using FertilityCare.Infrastructure.Configurations;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,14 +26,36 @@ namespace FertilityCare.Infrastructure.Services
 
     public class JwtService : IJwtService
     {
+        private readonly JwtConfiguration _jwtConfig;
+
+        public JwtService(IOptions<JwtConfiguration> jwtConfiguration)
+        {
+            _jwtConfig = jwtConfiguration.Value;
+        }
+
+
         public string GenerateAccessToken(IEnumerable<Claim> claims)
         {
-            throw new NotImplementedException();
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtConfig.SecretKey));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtConfig.Issuer,
+                audience: _jwtConfig.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtConfig.ExpirationInMinutes),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
         public string GenerateRefreshToken(IEnumerable<Claim> claims)
         {
-            throw new NotImplementedException();
+            var randomNumber = new byte[64];
+            using var r = RandomNumberGenerator.Create();
+            r.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
         }
 
         public ClaimsPrincipal? GetPrincipalFromExpiredToken(string token)
