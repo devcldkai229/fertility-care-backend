96dea61817fed1302143e344a00df06e87d5c0c6
devcldkai229 <kag40222@gmail.com>
2025-06-15 19:17:31 +0700

    add: Methods to implements jwt

--- FertilityCare.Infrastructure/Services/IJwtService.cs
+++ FertilityCare.Infrastructure/Services/IJwtService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,36 @@ namespace FertilityCare.Infrastructure.Services
 {
     public interface IJwtService
     {
+        string GenerateAccessToken(IEnumerable<Claim> claims);
 
+        string GenerateRefreshToken(IEnumerable<Claim> claims);
 
+        ClaimsPrincipal? GetPrincipalFromExpiredToken(string token);
+
+        bool ValidateToken(string token);
+
+    }
+
+    public class JwtService : IJwtService
+    {
+        public string GenerateAccessToken(IEnumerable<Claim> claims)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateRefreshToken(IEnumerable<Claim> claims)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClaimsPrincipal? GetPrincipalFromExpiredToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateToken(string token)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
