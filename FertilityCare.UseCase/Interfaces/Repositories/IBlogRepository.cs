2dccdde3ada7b2b005dd3c14563bc905dc932c75
Trung Nguyen <nguyen18nt@gmail.com>
2025-06-16 20:00:00 +0700

    add: DTO, Mapper of blog and feedback

---
+++ FertilityCare.UseCase/Interfaces/Repositories/IBlogRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FertilityCare.Domain.Entities;
+
+namespace FertilityCare.UseCase.Interfaces.Repositories
+{
+    public interface IBlogRepository : IBaseRepository<Blog, Guid>
+    {
+        public Task<List<Blog>> GetBlogByDoctor
+    }
+}
