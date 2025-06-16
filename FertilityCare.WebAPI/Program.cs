a5ce41a6b59e3c7c3dee23272924a0f96eed84ba
devcldkai229 <kag40222@gmail.com>
2025-06-16 18:27:03 +0700

    add: DbSeeder Config

--- FertilityCare.WebAPI/Program.cs
+++ FertilityCare.WebAPI/Program.cs
@@ -16,35 +16,32 @@ namespace FertilityCare.WebAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args) // Fix CS4033 by marking Main as async and changing return type to Task
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
 
             builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("SmtpSettings"));
-
             builder.Services.Configure<JwtConfiguration>(builder.Configuration.GetSection("JwtSettings"));
-
             builder.Services.Configure<GoogleAuthConfiguration>(builder.Configuration.GetSection("GoogleAuth"));
 
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowClient", policy =>
                 {
-                    policy.WithOrigins("http://localhost:5174", "http://localhost:5173") 
+                    policy.WithOrigins("http://localhost:5174", "http://localhost:5173")
                           .AllowAnyHeader()
                           .AllowAnyMethod()
-                          .AllowCredentials(); 
+                          .AllowCredentials();
                 });
             });
 
             builder.Services.AddDbContext<FertilityCareDBContext>(options =>
                     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
                            .UseLazyLoadingProxies());
-            
+
             var jwtConfig = builder.Configuration.GetSection(JwtConfiguration.SectionName).Get<JwtConfiguration>();
 
             builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
@@ -94,67 +91,60 @@ namespace FertilityCare.WebAPI
             });
 
             builder.Services.AddScoped<IDoctorService, DoctorService>();
-
             builder.Services.AddScoped<IDoctorScheduleService, DoctorScheduleService>();
-
             builder.Services.AddScoped<IDoctorScheduleRepository, DoctorScheduleRepository>();
-
             builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
-
             builder.Services.AddScoped<ISlotRepository, SlotRepository>();
-
             builder.Services.AddScoped<ITreatmentServiceRepository, TreatmentServiceRepository>();
-
             builder.Services.AddScoped<ITreatmentStepRepository, TreatmentStepRepository>();
-
             builder.Services.AddScoped<IPublicTreatmentService, PublicTreatmentService>();
-
             builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-
             builder.Services.AddScoped<IPrescriptionRepository, PrescriptionRepository>();
-
             builder.Services.AddScoped<IPrescriptionService, PrescriptionService>();
-
             builder.Services.AddScoped<IPatientService, PatientService>();
-
             builder.Services.AddScoped<IPatientRepository, PatientRepository>();
-
             builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
-
             builder.Services.AddScoped<IAppointmentReminderRepository, AppointmentReminderRepository>();
-
             builder.Services.AddScoped<IOrderStepRepository, OrderStepRepository>();
-
             builder.Services.AddScoped<IUserProfileRepository, UserProfileRepository>();
-
             builder.Services.AddScoped<IAppointmentService, AppointmentService>();
-
             builder.Services.AddScoped<IOrderService, OrderService>();
-
             builder.Services.AddScoped<IOrderStepService, OrderStepService>();
-
             builder.Services.AddScoped<IEmailService, EmailService>();
-
             builder.Services.AddScoped<IJwtService, JwtService>();
-
             builder.Services.AddScoped<IAuthService, AuthService>();
-
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+                await SeedRolesAsync(roleManager);
+            }
 
             app.UseHttpsRedirection();
-
             app.UseCors("AllowClient");
-
             app.UseAuthentication();
-
             app.UseAuthorization();
-
             app.MapControllers();
+            await app.RunAsync();
+        }
 
-            app.Run();
+        private static async Task SeedRolesAsync(RoleManager<IdentityRole<Guid>> roleManager)
+        {
+            var roles = new[] { "Admin", "Doctor", "Patient" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole<Guid> { Name = role });
+                }
+            }
         }
     }
+
+
 }
